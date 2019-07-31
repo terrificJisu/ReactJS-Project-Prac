c222946cfe5a6ab4f3d6451c43bd326c7ca7d817
danyelanshil <danyelanshil@gmail.com>
2019-07-31 15:39:54 +1000

    resort

---
+++ src/pages/Home.js
@@ -0,0 +1,17 @@
+// import React from 'react'
+
+// const Home = () => {
+//     return (
+//         <div>
+
+//         </div>
+//     )
+// }
+
+// export default Home
+
+import React from "react";
+
+export default function Home() {
+    return <div>hello from home page</div>
+}
