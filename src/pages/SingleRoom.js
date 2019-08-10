9e34cc9ff82a4f1c33a6dec09b939cb1a6cfa72f
danyelanshil <danyelanshil@gmail.com>
2019-08-10 22:32:02 +1000

    styledhero

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -4,19 +4,52 @@ import Hero from '../components/Hero';
 import Banner from '../components/Banner';
 import { Link } from 'react-router-dom';
 import { RoomContext } from '../context';
-
+import StyledHero from '../components/StyledHero';
 
 export default class SingleRoom extends Component {
     constructor(props) {
         super(props)
         // console.log(this.props);
+        this.state = {
+            slug: this.props.match.params.slug,
+            defualtBcg
+        };
     }
     
+    static contextType = RoomContext;
     componentDidMount(){
 
     }
 
     render() {
-        return <div>hello from single room page</div>;
+        const { getRoom } = this.context;
+        const room = getRoom(this.state.slug);
+        
+        if(!room) {
+            return <div className="error">
+                <h3>no such room could be found...</h3>
+                <Link to='/rooms' className="btn-primary">
+                    back to rooms
+                </Link>
+            </div>;
+        }
+
+        const { name, 
+            description, 
+            capacity, 
+            size, 
+            price, 
+            extras, 
+            breakfast, 
+            pets, 
+            images } = room;
+
+        return <StyledHero img={images[0] || this.state.defualtBcg}>
+            <Banner title={`${name} room`}>
+                <Link to='/rooms' className="btn-primary">
+                    back to rooms
+                </Link>
+            </Banner>
+        </StyledHero>;
     }
 }
